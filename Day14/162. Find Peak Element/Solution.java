cb4e9344712ca6daa954884bff8945bc9140dd1e
yashlondhe90960 <yashlondhe9151@gmail.com>
2024-07-30 22:22:49 +0530

    Day 14 completed

--- Day14/162. Find Peak Element/Solution.java
+++ Day14/162. Find Peak Element/Solution.java
@@ -1,16 +1,28 @@
 class Solution {
-    public long findMinDiff(ArrayList<Integer> a, int n, int m) {
-        // your code here
-        long res = Integer.MAX_VALUE;
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        if (n == 1)
+            return 0;
 
-        Collections.sort(a);
+        if (nums[0] > nums[1])
+            return 0;
+        if (nums[n - 1] > nums[n - 2])
+            return n - 1;
 
-        for (int i = 0; i < n - m + 1; i++) {
-            int minElement = a.get(i);
-            int maxElement = a.get(i + m - 1);
-            res = Math.min(res, maxElement - minElement);
+        int low = 1, high = n - 2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (nums[mid] > nums[mid - 1])
+                low = mid + 1;
+            else
+                high = mid - 1;
 
         }
-        return res;
+
+        return -1;
+
     }
 }
